c39a8ee84b893885e7fd5659f96cf096a6538a5a
Marcelo Carvalho <marcelo_scarvalho@outlook.com>
2022-10-12 15:14:35 -0300

    Criando componentes no storybook

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,26 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+    colors: {
+      white: '#fff',
+      black: '#000',
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7C7C8A',
+      'gray-200': '#C4C4CC',
+      'gray-100': '#E1E1E6',
+
+      'cyan-500': '#81D8F7',
+      'cyan-300': '#A2E0F7',
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
