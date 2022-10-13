63b37916fe9b08f55f8bce8908d16e2db9c1be4b
Marcelo Carvalho <marcelo_scarvalho@outlook.com>
2022-10-13 11:50:37 -0300

    Executado build no storybook

---
+++ storybook-static/assets/preview.a074a8b2.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.21a8b9ab.js";import"./iframe.1a69776a.js";import"./es.map.constructor.39ab096f.js";import"./es.number.to-fixed.5319baa5.js";import"./index.12d44c7a.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.a074a8b2.js.map
